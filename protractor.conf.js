82700b057789d5af194fad6ecc4c39a47cb38b06
dhiren <dhiren.patel@routerabbit.com>
2015-12-23 13:04:52 +0530

    Updated with html reports generation

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,4 +1,13 @@
+var Jasmine2HtmlReporter = require('protractor-jasmine2-html-reporter');
+
 exports.config = {
-  seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['protractor/todo-spec.js']
+    seleniumAddress: 'http://localhost:4444/wd/hub',
+    specs: ['protractor/todo-spec.js'],
+    onPrepare: function () {
+        jasmine.getEnv().addReporter(
+            new Jasmine2HtmlReporter({
+                savePath: 'target'
+            })
+        );
+    }
 };
--- protractor/todo-spec.js
+++ protractor/todo-spec.js
@@ -1,20 +1,46 @@
-describe('angularjs homepage todo list', function() {
-  it('should add a todo', function() {
-    browser.get('http://localhost:9000');
+describe('angularjs homepage todo list', function () {
+    it('should add a todo', function () {
+        browser.get('http://localhost:9000');
 
-    expect(browser.getTitle()).toEqual('http://localhost:9000/#/');
-    //element(by.model('username')).sendKeys('Shivam');
+        expect(browser.getTitle()).toEqual('http://localhost:9000/#/');
 
-    //element(by.model('todoList.todoText')).sendKeys('write first protractor test');
-    //element(by.css('[value="add"]')).click();
 
-    //var todoList = element.all(by.repeater('todo in todoList.todos'));
-    //expect(todoList.count()).toEqual(3);
-    //expect(todoList.get(2).getText()).toEqual('write first protractor test');
 
-    // You wrote your first test, cross it off the list
-    //todoList.get(2).element(by.css('input')).click();
-    //var completedAmount = element.all(by.css('.done-true'));
-    //expect(completedAmount.count()).toEqual(2);
-  });
+        //element(by.model('username')).sendKeys('Shivam');
+
+        //element(by.model('todoList.todoText')).sendKeys('write first protractor test');
+        //element(by.css('[value="add"]')).click();
+
+        //var todoList = element.all(by.repeater('todo in todoList.todos'));
+        //expect(todoList.count()).toEqual(3);
+        //expect(todoList.get(2).getText()).toEqual('write first protractor test');
+
+        // You wrote your first test, cross it off the list
+        //todoList.get(2).element(by.css('input')).click();
+        //var completedAmount = element.all(by.css('.done-true'));
+        //expect(completedAmount.count()).toEqual(2);
+    });
+
+    describe('angularjs about page', function () {
+        it('should add a todo', function () {
+            browser.get('http://localhost:9000/#/about');
+
+            expect(browser.getTitle()).toEqual('http://localhost:9000/#/about');
+
+
+            //element(by.model('username')).sendKeys('Shivam');
+
+            //element(by.model('todoList.todoText')).sendKeys('write first protractor test');
+            //element(by.css('[value="add"]')).click();
+
+            //var todoList = element.all(by.repeater('todo in todoList.todos'));
+            //expect(todoList.count()).toEqual(3);
+            //expect(todoList.get(2).getText()).toEqual('write first protractor test');
+
+            // You wrote your first test, cross it off the list
+            //todoList.get(2).element(by.css('input')).click();
+            //var completedAmount = element.all(by.css('.done-true'));
+            //expect(completedAmount.count()).toEqual(2);
+        });
+    });
 });
